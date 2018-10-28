e2236ecb3980d9b0c0c22e24704b69201b222ead
Mukund Ananthu <mukund.code@gmail.com>
2018-10-28 15:11:15 +0530

    Creating Room DB

---
+++ app/src/main/java/self/learning/db/WordRoomDatabase.java
@@ -0,0 +1,29 @@
+package self.learning.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import self.learning.dao.WordDao;
+import self.learning.entities.Word;
+
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    //provide abstract methods for each DAO that will be used by this DB
+    public abstract WordDao getWordDao();
+
+    // ensure this class is singleton to prevent multiple
+    // expensive instances of the db being opened at the same time
+    private static volatile WordRoomDatabase dbInstance;
+
+    static WordRoomDatabase getDataBase(final Context context) {
+        if(dbInstance == null) {
+            synchronized (WordRoomDatabase.class) {
+                dbInstance = Room.databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database").build();
+            }
+        }
+        return dbInstance;
+    }
+}
