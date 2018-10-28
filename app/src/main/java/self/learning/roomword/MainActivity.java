5d84f788e689fced4d1b22680fd6be6570eab8b0
Mukund Ananthu <mukund.code@gmail.com>
2018-10-28 18:47:30 +0530

    Add Recyclerview with adapter

--- app/src/main/java/self/learning/roomword/MainActivity.java
+++ app/src/main/java/self/learning/roomword/MainActivity.java
@@ -2,6 +2,10 @@ package self.learning.roomword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import self.learning.adapters.WordListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
