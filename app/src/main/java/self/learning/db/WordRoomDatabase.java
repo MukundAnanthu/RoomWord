d70c1f88e4d17355ad417dc25afdc11437b617cf
Mukund Ananthu <mukund.code@gmail.com>
2018-10-28 19:02:19 +0530

    Add db callback to delete old db data and prepopulate db with 2 words on each app start

--- app/src/main/java/self/learning/db/WordRoomDatabase.java
+++ app/src/main/java/self/learning/db/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package self.learning.db;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 import self.learning.dao.WordDao;
 import self.learning.entities.Word;
@@ -21,9 +24,42 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     public static WordRoomDatabase getDataBase(final Context context) {
         if(dbInstance == null) {
             synchronized (WordRoomDatabase.class) {
-                dbInstance = Room.databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database").build();
+                dbInstance = Room.databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database")
+                        .addCallback(sRoomDatabaseCallback)
+                        .build();
             }
         }
         return dbInstance;
     }
+
+    // delete all entries, add 2 words in DB on each app start
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(dbInstance).execute();
+                }
+            };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.getWordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+            Word word = new Word("Hello");
+            mDao.insert(word);
+            word = new Word("World");
+            mDao.insert(word);
+            return null;
+        }
+    }
+
 }
